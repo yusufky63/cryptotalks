ae9ea890bb4652c915a75ba2d889c5d7fa02289e
Yusuf <codexsha@gmail.com>
2023-01-21 22:10:01 +0300

    update

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -1,97 +1,167 @@
+/* eslint-disable quotes */
+/* eslint-disable no-shadow */
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
-import React, {useEffect, useState} from "react";
-import {View, FlatList, Text} from "react-native";
+import React, {useEffect, useState, useCallback} from "react";
+import {
+  View,
+  FlatList,
+  Text,
+  ToastAndroid,
+  PermissionsAndroid,
+} from "react-native";
 import {formatDistance, parseISO} from "date-fns";
 import {tr} from "date-fns/locale";
 import {styles} from "./Chat.style";
-
+import {GiftedChat, Send, Actions} from "react-native-gifted-chat";
+import Clipboard from "@react-native-clipboard/clipboard";
 import auth from "@react-native-firebase/auth";
-import database from "@react-native-firebase/database";
-import parseContentData from "../../utils/parseContentData";
+import Icon from "react-native-vector-icons/FontAwesome";
 import MessageCard from "../../components/MessageCard";
 import MessageTextModal from "../../modal/MessageTextModal";
+import database from "@react-native-firebase/database";
+import firestore from "@react-native-firebase/firestore";
 import {ActivityIndicator} from "react-native";
-
+import {launchCamera, launchImageLibrary} from "react-native-image-picker";
+import storage from "@react-native-firebase/storage";
+import ErrorMessages from "../../utils/ErrorMessages";
+import {ErrorShowMessage} from "../../utils/ErrorShowMessage";
 function Chat({navigation, route}) {
   const [messages, setMessages] = useState([]);
-  const [message, setMessage] = useState("");
-  const [loading, setLoading] = useState(false);
-
   const room = route.params;
-
-  const formattedDate = formatDistance(parseISO(room.createdAt), new Date(), {
-    addSuffix: true,
-    locale: tr,
-  });
 
   useEffect(() => {
     navigation.setOptions({title: route.params.name});
 
-    setLoading(true);
-    getMessages();
-    setLoading(false);
-  }, []);
+    return firestore()
+      .doc(`ChatRooms/` + room.id)
+      .onSnapshot((snapshot) => {
+        setMessages(snapshot.data()?.messages ?? []);
+      });
+  }, [room.id]);
 
-  function handleRefresh() {
-    getMessages();
-  }
+  const handleUploadImage = (image) => {
+    const uploadTask = storage()
+      .ref(`images/${image.fileName}`)
+      .putFile(image.uri);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case storage.TaskState.PAUSED:
+            console.log("Upload is paused");
+            break;
+          case storage.TaskState.RUNNING:
+            console.log("Upload is running");
 
-  function getMessages() {
-    console.log("getMessages");
-    database()
-      .ref(`/ChatRooms/${room.id}/Messages`)
-      .on("value", (snapshot) => {
-        const data = snapshot.val();
-        if (data) {
-          const parsedData = parseContentData(data);
-          setMessages(parsedData);
+            break;
         }
-      });
-  }
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+          console.log("File available at", downloadURL);
+          onSend([
+            {
+              _id: Math.round(Math.random() * 1000000),
+              image: downloadURL,
+              createdAt: new Date(),
+              user: {
+                _id: auth().currentUser.uid,
+                name: auth().currentUser.displayName,
+              },
+            },
+          ]);
+        });
+      }
+    );
+  };
 
-  function handleSend() {
-    console.log("send istek");
-    database()
-      .ref(`/ChatRooms/${room.id}/Messages`)
-      .push({
-        text: message,
-        createdAt: new Date().toISOString(),
-        createdName: auth().currentUser.email.split("@")[0],
-      });
-    console.log("send gödenrildi");
-    getMessages();
-    this.flatListRef.scrollToEnd({animated: true});
+  const handleSelectImage = () => {
+    launchImageLibrary({mediaType: "photo"}, (response) => {
+      if (response.didCancel) {
+        console.log("User cancelled image picker");
+      } else if (response.error) {
+        console.log("ImagePicker Error: ", response.error);
+      } else if (response.customButton) {
+        console.log("User tapped custom button: ", response.customButton);
+      } else {
+        handleUploadImage(response.assets[0]);
+      }
+    });
+  };
 
-    setMessage("");
-  }
+  const onSend = (m = []) => {
+    firestore()
+      .doc("ChatRooms/" + room.id)
+      .set(
+        {
+          messages: GiftedChat.append(messages, m),
+        },
+        {merge: true}
+      );
+  };
 
-  const renderItem = ({item}) => <MessageCard message={item} />;
+  // const renderItem = ({item}) => <MessageCard message={item} />;
 
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Oluşturan: {room.createdName} </Text>
-        <Text style={styles.headerText}>
-          Oluşturulma Tarihi: {formattedDate}
-        </Text>
-      </View>
-      <View style={styles.flatList}>
-        <FlatList
-          ref={(ref) => {
-            this.flatListRef = ref;
-          }}
-          onRefresh={handleRefresh}
-          refreshing={loading}
-          scrollEnabled={true}
-          data={messages}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.createdAt}
-        />
-      </View>
-      <MessageTextModal
-        SendMessage={handleSend}
-        setMessage={setMessage}
-        message={message}
+    <View style={{flex: 1, backgroundColor: "white"}}>
+      <GiftedChat
+        messages={messages.map((x) => ({
+          ...x,
+          createdAt: x.createdAt?.toDate(),
+        }))}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: auth().currentUser.uid,
+          name: auth().currentUser.displayName,
+          avatar: auth().currentUser.photoURL,
+        }}
+        showAvatarForEveryMessage={true}
+        isTyping={true}
+        placeholder="Mesajınızı yazın..."
+        alwaysShowSend={true}
+        timeFormat="HH:mm"
+        renderUsernameOnMessage={true}
+        renderAvatarOnTop={true}
+        ActivityIndicator={true}
+        renderSend={(props) => (
+          <Send
+            {...props}
+            containerStyle={{
+              height: 50,
+              width: 50,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Icon name="send" size={25} color="#2e64e5" />
+          </Send>
+        )}
+        renderActions={(props) => (
+          <Actions
+            {...props}
+            containerStyle={{
+              height: 50,
+              width: 50,
+              justifyContent: "center",
+              alignItems: "center",
+              marginBottom: 0,
+            }}
+            options={{
+              "Galeriden Gönder": handleSelectImage,
+              "Kameradan Gönder": handleSelectImage,
+              İptal: () => {},
+            }}
+            icon={() => <Icon name="plus" size={25} color="#2e64e5" />}
+            optionTintColor="#222B45"
+          />
+        )}
       />
     </View>
   );
